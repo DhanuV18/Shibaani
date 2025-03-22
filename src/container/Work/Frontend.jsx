6d99fd6f607b75bc822c77d80ab063a043e1a1fa
Dhanushan <dhanushan.info@gmail.com>
2025-03-22 23:38:10 +0530

    updated

--- src/container/Work/Frontend.jsx
+++ src/container/Work/Frontend.jsx
@@ -4,9 +4,11 @@ import { motion } from 'framer-motion';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 
 
-import movie from '../../images/thumbnail/movie.webp'
-import tesla from '../../images/thumbnail/tesla.webp'
-import scooby from '../../images/thumbnail/scooby.webp'
+import movie from '../../images/thumbnail/movie@2x.webp'
+import tesla from '../../images/thumbnail/tesla@2x.webp'
+import scooby from '../../images/thumbnail/scooby@2x.webp'
+
+
 
 function Frontend({ index, imgUrl, name, projectLink, codeLink, tools, title, description}) {
 
